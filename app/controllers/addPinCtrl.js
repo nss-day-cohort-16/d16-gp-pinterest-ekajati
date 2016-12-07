e2592af344bbd33b07877ea7f00e4e603b4efcd3
Nick Chemsak <nchemsak@gmail.com>
2016-12-07 09:21:12 -0600

    small addPin fix

--- app/controllers/addPinCtrl.js
+++ app/controllers/addPinCtrl.js
@@ -1,24 +1,20 @@
-// "use strict";
+"use strict";
 
-// app.controller("addPinCtrl", function($scope) {
-//       //add in filter and auth factories
+app.controller("addPinCtrl", function($scope) {
+      //add in filter and auth factories
 
-//       $scope.title = "Add a new pin";
-//       $scope.btnText = "Save new pin";
+      $scope.title = "Add a new pin";
+      $scope.btnText = "Save new pin";
 
-//       $scope.addNewPin = {
-//         assignedTo: "",
-//         dependencies: "",
-//         dueDate: "",
-//         location: "",
-//         task: "",
-//         urgency: "low",
-//         isCompleted: false,
-//         uid: AuthFactory.loginUser
-//       };
+      $scope.addNewPin = {
+        description: "",
+        URL: "",
+        board: "",
+        uid: authFactory.loginUser
+      };
 
-//       $scope.addNewPin = function() {
-//         console.log("you clicked on add new pin:", $scope.addNewPin);
+      $scope.addNewPin = function() {
+        console.log("you clicked on add new pin:", $scope.addNewPin);
 
-//       };
-//     };
+      };
+    };
