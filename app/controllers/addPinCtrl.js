53bf5c8797b8ecd940828f4ed66d6302f305bd83
Nick Chemsak <nchemsak@gmail.com>
2016-12-07 09:11:23 -0600

    add pins

---
+++ app/controllers/addPinCtrl.js
@@ -0,0 +1,24 @@
+// "use strict";
+
+// app.controller("addPinCtrl", function($scope) {
+//       //add in filter and auth factories
+
+//       $scope.title = "Add a new pin";
+//       $scope.btnText = "Save new pin";
+
+//       $scope.addNewPin = {
+//         assignedTo: "",
+//         dependencies: "",
+//         dueDate: "",
+//         location: "",
+//         task: "",
+//         urgency: "low",
+//         isCompleted: false,
+//         uid: AuthFactory.loginUser
+//       };
+
+//       $scope.addNewPin = function() {
+//         console.log("you clicked on add new pin:", $scope.addNewPin);
+
+//       };
+//     };
